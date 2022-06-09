509ed4021f953f1384cde9ba017790c7bd7d349a
Gustavo-Blois <104032155+Gustavo-Blois@users.noreply.github.com>
2022-06-09 18:19:55 -0300

    Update A.c

--- A.c
+++ A.c
@@ -3,44 +3,48 @@
 int main(){
     char matriz[3][1000];
     char string[3000];
-    int tamanho[3];
     int espacos = 0;
     int largura;
-    int endereco[100];
-    char palavra[20][20];
-    int k = 0;
-    int n = 0;
+    char aux[20][20];
+    int contador = 0;
     for(int i = 0; i<3;i++){
     fgets(matriz[i], 1000, stdin);
     }
-    for(int i=0;i<3;i++){
-        tamanho[i] = strlen(matriz[i]);
-    }
+  
     //lê a matriz de char
     //agora a gente vai criar uma única string a partir disso
     for(int i=0;i<3;i++){
-    strncat(string, matriz[i],tamanho[i]);
+    strcat(string, matriz[i]);
     }
     largura = strlen(string);
-
-    for(int i = 0; i <largura; k++){
-        while(string[i] != 32){
-            palavra[k][i] = string[i];
-            i++;
+    for(int i = 0; i < largura; i++){
+        if(string[i] == '\n'){
+            string[i] = 32;
         }
-        i++;
     }
-    for(int i = 0;i<largura;i++){
+    largura = strlen(string);
+     printf("%s",string);
+    for(int i = 0;string[i] != '\0';i++){
         if(string[i] == 32){
             espacos++;
         }
     }
-    n = espacos + 6;
-    for(int i = 0; i < n;i++){
-        for(int j = i+1; j < n;j++){
-        if(strcmp(palavra[i],palavra[j]) == 0){n--;};
-        }
+int k = 0;
+int l = 0;
+for(int i = 0; string[i] != '\0';i++){
+l = 0;
+while(string[i] != 32){
+aux[k][l] = string[i];
+l++;
+}
+k++;
+}
+for(int n = 0; n < k; n++){
+    for(int m = n+1; m < k;m++){
+if(strcmp(aux[n],aux[m])!= 0){contador += 1;}
     }
-printf("%d\n",n);
+}
+
+printf("%d\n",contador);
     return 0;
 }
